8085bd3d64b52373eee58708f3df9fc28a41e202
sava <savamedvedev@gmail.com>
2022-08-10 06:00:43 +0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import styled, { ThemeProvider } from "styled-components";
+import { Routes, Route } from "react-router-dom";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
+
+function App() {
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          <Routes>
+            <Route path="/"></Route>
+          </Routes>
+        </Wrapper>
+      </Main>
+    </Container>
+  );
+}
+
+export default App;
